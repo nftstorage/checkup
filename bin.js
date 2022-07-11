4799549be613ba712e1b3b7c6a2e9e05c5fd64f2
Alan Shaw <alan.shaw@protocol.ai>
2022-07-11 18:03:18 +0000

    refactor: catch peer selection error

--- bin.js
+++ bin.js
@@ -5,35 +5,23 @@ import { startCheckup } from './index.js'
 
 dotenv.config()
 
-async function main () {
-  while (true) {
-    try {
-      await startCheckup({
-        dbConnString: mustGetEnv('DATABASE_CONNECTION'),
-        ipfsCheckEndpoint: mustGetEnv('IPFS_CHECK_API_URL'),
-        clusterEndpoint: mustGetEnv('CLUSTER_API_URL'),
-        clusterBasicAuthToken: mustGetEnv('CLUSTER_BASIC_AUTH_TOKEN'),
-        sampleMethod: process.env.SAMPLE_METHOD,
-        port: process.env.PORT,
-        elasticProvider: process.env.ELASTIC_PROVIDER_ADDR
-          ? {
-              multiaddr: mustGetEnv('ELASTIC_PROVIDER_ADDR'),
-              s3Region: mustGetEnv('ELASTIC_PROVIDER_S3_REGION'),
-              s3Bucket: mustGetEnv('ELASTIC_PROVIDER_S3_BUCKET'),
-              s3AccessKeyId: mustGetEnv('ELASTIC_PROVIDER_S3_ACCESS_KEY_ID'),
-              s3SecretAccessKey: mustGetEnv('ELASTIC_PROVIDER_S3_SECRET_ACCESS_KEY')
-            }
-          : undefined
-      })
-    } catch (err) {
-      console.error(err)
-      console.log('Waiting 30s before restart...')
-      await new Promise(resolve => setTimeout(resolve, 30_000))
-    }
-  }
-}
-
-main()
+startCheckup({
+  dbConnString: mustGetEnv('DATABASE_CONNECTION'),
+  ipfsCheckEndpoint: mustGetEnv('IPFS_CHECK_API_URL'),
+  clusterEndpoint: mustGetEnv('CLUSTER_API_URL'),
+  clusterBasicAuthToken: mustGetEnv('CLUSTER_BASIC_AUTH_TOKEN'),
+  sampleMethod: process.env.SAMPLE_METHOD,
+  port: process.env.PORT,
+  elasticProvider: process.env.ELASTIC_PROVIDER_ADDR
+    ? {
+        multiaddr: mustGetEnv('ELASTIC_PROVIDER_ADDR'),
+        s3Region: mustGetEnv('ELASTIC_PROVIDER_S3_REGION'),
+        s3Bucket: mustGetEnv('ELASTIC_PROVIDER_S3_BUCKET'),
+        s3AccessKeyId: mustGetEnv('ELASTIC_PROVIDER_S3_ACCESS_KEY_ID'),
+        s3SecretAccessKey: mustGetEnv('ELASTIC_PROVIDER_S3_SECRET_ACCESS_KEY')
+      }
+    : undefined
+})
 
 /**
  * @param {string} name
--- peer.js
+++ peer.js
@@ -30,57 +30,61 @@ export function selectPeer (cluster, elasticProvider) {
   return async function * (source) {
     for await (const sample of source) {
       const peerType = elasticProvider ? randomPeerType() : CLUSTER_PEER
-      if (peerType === CLUSTER_PEER) {
-        let status = await clusterStatusWithTimeout(cluster, sample.cid)
-        let pinInfos = Object.values(status.peerMap)
-        if (pinInfos.every(e => e.status === 'unpinned')) {
-          log(`⚠️ ${status.cid} is not pinned on ANY peer!`)
-          let otherCid
-          try {
-            otherCid = toOtherCidVersion(status.cid)
-          } catch {
-            yield /** @type {Sample} */ ({ cid: status.cid })
-            continue
-          }
-
-          log(`trying other CID version: ${otherCid}`)
-          status = await clusterStatusWithTimeout(cluster, otherCid)
-          pinInfos = Object.values(status.peerMap)
+      try {
+        if (peerType === CLUSTER_PEER) {
+          let status = await clusterStatusWithTimeout(cluster, sample.cid)
+          let pinInfos = Object.values(status.peerMap)
           if (pinInfos.every(e => e.status === 'unpinned')) {
-            log(`⚠️ ${otherCid} is not pinned on ANY peer!`)
+            log(`⚠️ ${status.cid} is not pinned on ANY peer!`)
+            let otherCid
+            try {
+              otherCid = toOtherCidVersion(status.cid)
+            } catch {
+              yield /** @type {Sample} */ ({ cid: status.cid })
+              continue
+            }
+
+            log(`trying other CID version: ${otherCid}`)
+            status = await clusterStatusWithTimeout(cluster, otherCid)
+            pinInfos = Object.values(status.peerMap)
+            if (pinInfos.every(e => e.status === 'unpinned')) {
+              log(`⚠️ ${otherCid} is not pinned on ANY peer!`)
+              yield /** @type {Sample} */ ({ cid: status.cid })
+              continue
+            }
+          }
+
+          // pin information where:
+          // status != remote (pinned on another peer in the cluster)
+          // status != pin_queued (may not be available on this peer yet)
+          const eligiblePinInfos = pinInfos
+            .filter(i => i.status !== 'remote' && i.status !== 'pin_queued')
+
+          if (!eligiblePinInfos.length) {
+            log(`⚠️ ${status.cid} no eligible pin statuses: ${pinInfos.map(i => i.status)}`)
             yield /** @type {Sample} */ ({ cid: status.cid })
             continue
           }
+
+          const eligiblePeers = eligiblePinInfos.map(pi => `/p2p/${pi.ipfsPeerId}`)
+          const index = randomInt(0, eligiblePeers.length)
+
+          log(`sample ready: ${status.cid} @ ${eligiblePeers[index]} (${eligiblePinInfos[index].status})`)
+          yield /** @type {PeeredSample} */ ({ cid: status.cid, peer: eligiblePeers[index] })
+        } else if (peerType === ELASTIC_PROVIDER_PEER) {
+          const hasCid = await elasticProvider.has(sample.cid)
+          if (!hasCid) {
+            log(`⚠️ ${sample.cid} not available on Elastic Provider`)
+            yield /** @type {Sample} */ ({ cid: sample.cid })
+            continue
+          }
+          log(`sample ready: ${sample.cid} @ ${elasticProvider.multiaddr} (found on S3)`)
+          yield /** @type {PeeredSample} */ ({ cid: sample.cid, peer: elasticProvider.multiaddr })
+        } else {
+          throw new Error(`unknown peer type: ${peerType}`)
         }
-
-        // pin information where:
-        // status != remote (pinned on another peer in the cluster)
-        // status != pin_queued (may not be available on this peer yet)
-        const eligiblePinInfos = pinInfos
-          .filter(i => i.status !== 'remote' && i.status !== 'pin_queued')
-
-        if (!eligiblePinInfos.length) {
-          log(`⚠️ ${status.cid} no eligible pin statuses: ${pinInfos.map(i => i.status)}`)
-          yield /** @type {Sample} */ ({ cid: status.cid })
-          continue
-        }
-
-        const eligiblePeers = eligiblePinInfos.map(pi => `/p2p/${pi.ipfsPeerId}`)
-        const index = randomInt(0, eligiblePeers.length)
-
-        log(`sample ready: ${status.cid} @ ${eligiblePeers[index]} (${eligiblePinInfos[index].status})`)
-        yield /** @type {PeeredSample} */ ({ cid: status.cid, peer: eligiblePeers[index] })
-      } else if (peerType === ELASTIC_PROVIDER_PEER) {
-        const hasCid = await elasticProvider.has(sample.cid)
-        if (!hasCid) {
-          log(`⚠️ ${sample.cid} not available on Elastic Provider`)
-          yield /** @type {Sample} */ ({ cid: sample.cid })
-          continue
-        }
-        log(`sample ready: ${sample.cid} @ ${elasticProvider.multiaddr} (found on S3)`)
-        yield /** @type {PeeredSample} */ ({ cid: sample.cid, peer: elasticProvider.multiaddr })
-      } else {
-        throw new Error(`unknown peer type: ${peerType}`)
+      } catch (err) {
+        log(new Error(`failed to select peer for sample: ${sample.cid}`, { cause: err }))
       }
     }
   }
